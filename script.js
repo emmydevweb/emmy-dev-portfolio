d903532f1da6f990410d9fc8f09c54c0b678cd08
Oduola Emmanuel <marvellousoduola@gmail.com>
2025-09-04 16:15:12 +0100

    Added header subtitle styling and fixed scroll-to-top button.

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-// Get the button element const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+// Get the button
+let mybutton = document.getElementById("scrollToTopBtn");
+
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopBtn.style.display = "block";
-} else {
-    scrollToTopBtn.style.display = "none";
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
-};
-// When the user clicks on the buttom, scroll to the top of the document scrollToTopBtn.addEvent
-AudioListener("click", () =>
-{
-    document.body.scrollTop = 0; // For safari
-    document.documentElement.ScrollTop = 0; // For Chrome, Firefox, IE and opera
-});
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+mybutton.addEventListener("click", topFunction);
